6491a73beed9e317e07e7d9099511983cc5a3296
Cody Clark <codyjamesclark@gmail.com>
2024-06-27 13:40:06 -0500

    started designing algorthim for governor and facility module

---
+++ scripts/facilities.js
@@ -0,0 +1,23 @@
+//import facility data from facility manager
+
+//define a function to get HTML for facility select element
+
+    //add change event listener handl facilityChoice
+
+    //save facility data to a varaible
+
+    //set variable html <h2>Choose facility</h2> <select name= facility">
+                    //<option disabled selected>Choose facility</option>
+
+    //map facilitys array
+
+        //return <option value= facilityId"> facility Name</option>
+
+    //join map
+    //close select
+
+    //return html
+
+//define event listener
+    //if target name === facility
+    //se facility(parseInt facilityId))
