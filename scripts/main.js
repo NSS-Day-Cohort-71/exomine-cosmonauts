df28ba82d20c5e5005091a9776b97bc5e02291fc
Cody Clark <codyjamesclark@gmail.com>
2024-06-28 11:52:26 -0500

    added change event listener to facilityMineral module

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@
 
 import { facilityList } from "./facilities.js"
 import { governorsList } from "./governors.js"
+import { spaceCartElement } from "./spaceCart.js"
 
 //facility select html
 
@@ -14,4 +15,12 @@ const mainHTML = document.querySelector("#main-container")
 mainHTML.innerHTML = `
 ${await governorsList()}
 ${await facilityList()}
+    <div id="colonyMineralContainer">
+            <h2>Colony Minerals</h2>
+        </div>
+    
+        <div id="facilityMineralContainer">
+            <h2>Facility Minerals</h2>
+        </div>
+${await spaceCartElement()}
 `
