d4104560f3f82d14ffd906394d6aade38cc1084c
pascual ramirez Valeriano <pascualramirez27@gmail.com>
2024-07-01 12:40:31 -0400

    reRenders when purchase button is clicked

--- scripts/facilityMinerals.js
+++ scripts/facilityMinerals.js
@@ -1,30 +1,31 @@
-import { getFacilityMinerals } from "./managers/facilityMineralsManager.js"
-import { facilityState, setMineralId } from "./TransientState.js"
+import { getFacilityMinerals } from "./managers/facilityMineralsManager.js";
+import { facilityState, setMineralId } from "./TransientState.js";
 
 export const facilityMineralList = async () => {
+  document.addEventListener("change", handleMineralChoice);
+  //set facilityMinerals to variable
+  const facilityMinerals = await getFacilityMinerals();
 
-    document.addEventListener("change", handleMineralChoice)
-    //set facilityMinerals to variable
-   const facilityMinerals = await getFacilityMinerals()
-
-
-   const facilityMineralHTML = facilityMinerals.map(facility => {
-       //if transientState.facilityId = facilityMinerals.facilityId
-       if (facilityState.facilityId === facility.facilityId) {
-        
-           return `
+  const facilityMineralHTML = facilityMinerals
+    .map((facility) => {
+      //if transientState.facilityId = facilityMinerals.facilityId
+      if (facilityState.facilityId === facility.facilityId) {
+        return `
                <h2>${facility.facility.name}</h2>
                <input type="radio" name="facilityMineral" value="${facility.mineralId}" data-mineral="${facility.mineral.name}" data-facility="${facility.facility.name}">${facility.mineralAmount} tons of ${facility.mineral.name}</input>
-           `
-       }
-   }).join("")
+           `;
+      }
+    })
+    .join("");
 
-   return facilityMineralHTML 
-}
+  return facilityMineralHTML;
+};
 
 export const handleMineralChoice = (e) => {
-    if (e.target.name="facilityMineral") {
-        setMineralId(e.target.value)
-        document.getElementById("mineralsInCart").innerHTML = `1 ton of ${e.target.dataset.mineral} from ${e.target.dataset.facility}`
-    }
-}
+  if ((e.target.name = "facilityMineral")) {
+    setMineralId(e.target.value);
+    document.getElementById(
+      "mineralsInCart"
+    ).innerHTML = `1 ton of ${e.target.dataset.mineral} from ${e.target.dataset.facility}`;
+  }
+};
